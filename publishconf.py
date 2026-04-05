47d943960b2f3094ebe0408ece1a9731b56ae6ff
Melcore <lapostolle.jean@gmail.com>
2026-04-06 01:38:47 +0200

    feat: cnames

--- publishconf.py
+++ publishconf.py
@@ -8,13 +8,19 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://jeanlapostolle.github.io"
+SITEURL = "https://www.melcore.fr"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
+
+# Static files for custom domain
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {
+    'extra/CNAME': {'path': 'CNAME'},
+}
 
 # Following items are often useful when publishing
 
